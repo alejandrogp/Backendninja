15c16510051492ab2da17cd594a0e3f3163654a8
alejandro <alejandrogp@gmail.com>
2018-06-11 23:09:13 +0200

    Insertando listados en plantillas

--- src/main/java/com/udemy/controller/ExampleController.java
+++ src/main/java/com/udemy/controller/ExampleController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/example")
 public class ExampleController {
@@ -18,7 +21,8 @@ public class ExampleController {
     @GetMapping("/exampleString")
     //@RequestMapping(value="exampleString", method = RequestMethod.GET)
     public String exampleString(Model model){
-        model.addAttribute("person", new Person("Alejandro",36));
+        //model.addAttribute("person", new Person("Alejandro",36));
+        model.addAttribute("people", getPeople());
         return EXAMPLE_VIEW;
     }
 
@@ -27,8 +31,19 @@ public class ExampleController {
     //@RequestMapping(value="exampleMAV", method = RequestMethod.GET)
     public ModelAndView exampleMAV(){
         ModelAndView modelAndView = new ModelAndView(EXAMPLE_VIEW);
-        modelAndView.addObject("person", new Person("Alberto",30));
+        //modelAndView.addObject("person", new Person("Alberto",30));
+        modelAndView.addObject("people", getPeople());
         return modelAndView;
     }
 
+
+    private List<Person> getPeople(){
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("Alejandro",36));
+        people.add(new Person("Alberto",30));
+        people.add(new Person("Pedro",43));
+        people.add(new Person("Maria",23));
+        people.add(new Person("Juan",29));
+        return people;
+    }
 }
