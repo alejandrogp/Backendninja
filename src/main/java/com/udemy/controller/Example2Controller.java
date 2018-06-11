ed7e3c2d6b38d55ba77f7ead8089febd415c85a6
alejandro <alejandrogp@gmail.com>
2018-06-11 23:29:34 +0200

    Recibiendo una petición GET (Forma 2)

--- src/main/java/com/udemy/controller/Example2Controller.java
+++ src/main/java/com/udemy/controller/Example2Controller.java
@@ -2,6 +2,7 @@ package com.udemy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -20,4 +21,14 @@ public class Example2Controller {
         mav.addObject("nm_in_model",name);
         return mav;
     }
+
+    //localhost:8080/example2/request2/jon
+    //localhost:8080/example2/request2/mikel
+    @GetMapping("/request2/{nm}")
+    public ModelAndView request2(@PathVariable("nm") String name){
+        ModelAndView mav = new ModelAndView(EXAMPLE2_VIEW);
+        mav.addObject("nm_in_model",name);
+        return mav;
+    }
+
 }
